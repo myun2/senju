ef0d690a9a5b2b580fed04fba259d355df7b2e5a
myun2 <myun2@nwhite.info>
2019-01-11 22:39:16 +0900

    Assignee, Estimate, Priority

--- models.go
+++ models.go
@@ -5,18 +5,49 @@ import (
   _ "github.com/lib/pq"
 )
 
+type Project struct {
+  gorm.Model
+  Name string `gorm:"not null"`
+  Tasks []Task
+}
+
 type Task struct {
   gorm.Model
-  Title   string `gorm:"not null"`
-  Content string
+  ProjectID     uint `gorm:"index;not null"`
+  Title       string `gorm:"not null"`
+  Description string
+  Priority   float32 `gorm:"not null;default 1"`
+
   TaskDeadline TaskDeadline
-  Labels []Label
+  TaskAssignee TaskAssignee
+  TaskEstimate TaskEstimate
+  Comments     []Comment
+  Labels       []Label
 }
 
 type TaskDeadline struct {
   gorm.Model
   TaskID    uint `gorm:"unique;index;not null"`
   Time time.Time `gorm:"not null"`
+}
+
+type TaskEstimate struct {
+  gorm.Model
+  TaskID   uint `gorm:"unique;index;not null"`
+  Value float32 `gorm:"not null"`
+}
+
+type TaskAssignee struct {
+  gorm.Model
+  TaskID  uint `gorm:"unique;index;not null"`
+  Name  string `gorm:"size:255;not null"`
+}
+
+type Comment struct {
+  gorm.Model
+  TaskID  uint `gorm:"unique;index;not null"`
+  Name  string `gorm:"size:255;not null"`
+  Content string `gorm:"not null"`
 }
 
 type Label struct {
@@ -43,14 +74,28 @@ func DbOpen() *gorm.DB {
 
 func DbMigrate(db *gorm.DB) {
   db.AutoMigrate(
-    &Task{}, &Label{}, &TaskDeadline{}, &TaskLabel{})
+    &Project{}, &Task{}, &Label{}, &TaskLabel{}, &Comment{},
+    &TaskDeadline{}, &TaskAssignee{}, &TaskEstimate{})
 
-  db.Debug().Model(&TaskDeadline{}).AddForeignKey(
-    "task_id", "tasks(id)", "RESTRICT", "RESTRICT")
+  // Foreign Keys
+  db.Model(&Task{}).AddForeignKey(
+    "project_id", "projects(id)", "RESTRICT", "RESTRICT")
 
-  db.Debug().Model(&TaskLabel{}).AddForeignKey(
-    "task_id",  "tasks(id)", "RESTRICT", "RESTRICT")
-  db.Debug().Model(&TaskLabel{}).AddForeignKey(
-    "label_id", "labels(id)", "RESTRICT", "RESTRICT")
-  db.Model(&TaskLabel{}).AddUniqueIndex("idx_task_id_and_label_id", "task_id", "label_id")
+  db.Model(&TaskDeadline{}).AddForeignKey(
+    "task_id",    "tasks(id)", "RESTRICT", "RESTRICT")
+  db.Model(&TaskAssignee{}).AddForeignKey(
+    "task_id",    "tasks(id)", "RESTRICT", "RESTRICT")
+  db.Model(&TaskEstimate{}).AddForeignKey(
+    "task_id",    "tasks(id)", "RESTRICT", "RESTRICT")
+  db.Model(&Comment{}).AddForeignKey(
+    "task_id",    "tasks(id)", "RESTRICT", "RESTRICT")
+
+  db.Model(&TaskLabel{}).AddForeignKey(
+    "task_id",    "tasks(id)", "RESTRICT", "RESTRICT")
+  db.Model(&TaskLabel{}).AddForeignKey(
+    "label_id",   "labels(id)", "RESTRICT", "RESTRICT")
+
+  // Multiple-key Unique Indexes
+  db.Model(&TaskAssignee{}).AddUniqueIndex("idx_task_id_and_assignee", "task_id", "name")
+  db.Model(&TaskLabel{}   ).AddUniqueIndex("idx_task_id_and_label_id", "task_id", "label_id")
 }
